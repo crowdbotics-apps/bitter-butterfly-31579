5241ae5a1434e3c58095dffe8dba72d3aa054df4
Crowdbotics <team@crowdbotics.com>
2021-10-20 01:44:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bitter_butterfly_31579",
+  url: "https://bitter_butterfly_31579.botics.co",
+  api: "https://bitter_butterfly_31579.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
